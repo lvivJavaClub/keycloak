934ef1f6ea093fa1e0f7ec649e9ad0d7610f4059
Ivan Verhun <902581+iverhun@users.noreply.github.com>
2019-08-15 11:10:53 +0300

    fetching JWT from keycloak

--- service/src/main/java/org/javaclub/keycloak/service/Main.java
+++ service/src/main/java/org/javaclub/keycloak/service/Main.java
@@ -1,11 +1,23 @@
 package org.javaclub.keycloak.service;
 
 import io.javalin.Javalin;
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.representations.idm.authorization.AuthorizationRequest;
+import org.keycloak.representations.idm.authorization.AuthorizationResponse;
 
 public class Main {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
         app.get("/", ctx -> ctx.result("Hello world"));
+
+        AuthzClient authzClient = AuthzClient.create();
+        AuthorizationRequest request = new AuthorizationRequest();
+
+        AuthorizationResponse response = authzClient.authorization("yura", "jclub")
+                .authorize(request);
+        String rpt = response.getToken();
+
+        System.out.println("You got an RPT: " + rpt);
     }
 }
